539e7a810331acc9ee6ec5ce0e88d11a809415ae
Sebin <sebinkuriakose276@gmail.com>
2023-06-18 19:51:44 +0530

    shortening

--- urban/app.py
+++ urban/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urban.db'
 db= SQLAlchemy(app)
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 @app.route('/home')
