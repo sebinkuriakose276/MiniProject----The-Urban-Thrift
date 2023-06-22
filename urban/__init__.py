ee1ae22dfed54e523372b2e8c99f7b5e5410b421
jazzirHussain <jasirhussain2002@gmail.com>
2023-06-23 02:35:21 +0530

    changes

---
+++ urban/__init__.py
@@ -0,0 +1,42 @@
+from flask import Flask,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urban.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view="login"
+login_manager.login_message_category="info"
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    # Redirect the user to the login page or display an error message
+    return redirect(url_for('auth.login'))
+
+
+# blueprint for auth routes in our app
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+# blueprint for non-auth parts of app
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     app.config['SECRET_KEY'] = 'secret-key-goes-here'
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///urban.db'
+
+#     db.init_app(app)
+
+#     login_manager = LoginManager()
+#     login_manager.login_view = 'auth.login'
+#     login_manager.init_app(app)
+
+
+
+      
+
+#     return app
--- urban/models.py
+++ urban/models.py
@@ -1,9 +1,37 @@
-from app import db
-#vbdjhfjkjfhvnxjnkjnjnjknk
-class Product(db.Model):
-    __tablename__ = 'products'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    description = db.Column(db.Text)
-    image_url = db.Column(db.String(200))
-    product_type = db.Column(db.String(50))
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from urban import db, login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+class Products(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    image= db.Column(db.String(length=30), nullable=False)
+    name= db.Column(db.String(length=30), nullable=False)
+    price= db.Column(db.Integer(), nullable=False)
+    type= db.Column(db.String(length=30), nullable=False)
+
+class Users(UserMixin, db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    username= db.Column(db.String(length=30), nullable=False)
+    phone= db.Column(db.String(length=30), nullable=False)
+    email= db.Column(db.String(length=30), nullable=False)
+    password = db.Column(db.String(100))
+
+class CartItems(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    product = db.relationship("Products", backref='product')
+    quantity = db.Column(db.Integer())
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+class Coupons(db.Model):
+    id=db.Column(db.Integer(), primary_key=True)
+    code = db.Column(db.String(length=10))
+    amount = db.Column(db.Integer())
+    status = db.Column(db.String, default='active')
+
+
+    
